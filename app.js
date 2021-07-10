3b60906fc575967088c6e9fcaefd64ccbcd30710
Yashkanekar <kanekaryash.11@gmail.com>
2021-07-10 18:19:54 +0530

    initial update

---
+++ app.js
@@ -0,0 +1,94 @@
+const cardButtons = document.querySelectorAll('.btn');
+const outermodal = document.querySelector('.outer-modal');
+const innermodal = document.querySelector('.inner-modal');
+let questionString; 
+let answerString;
+const revealAnswer = document.querySelector(".inner-btn");
+
+cardButtons.forEach((button) =>
+    button.addEventListener("click", handleClick)
+);
+function handleClick(event) {
+    const button = event.currentTarget;
+    const card = button.closest('.card');
+    const number = card.dataset.number;
+    const question = innermodal.querySelector(".question");
+    const answer = innermodal.querySelector(".answer");
+
+    switch(number){
+        case '1':
+            questionString  = "question 1"
+            answerString  = "answer 1"
+            break;
+        case '2':
+            questionString  = "question 2"
+            answerString  = "answer 2"
+            break;
+        case '3':
+            questionString  = "question 3"
+            answerString  = "answer 3"
+            break;
+        case '4':
+            questionString  = "question 4"
+            answerString  = "answer 4"
+            break;
+        case '5':
+            questionString  = "question 5"
+            answerString  = "answer 5"
+            break;
+        case '6':
+            questionString  = "question 6"
+            answerString  = "answer 6"
+            break;
+        case '7':
+            questionString  = "question 7"
+            answerString  = "answer 7"
+            break;
+        case '8':
+            questionString  = "question 8"
+            answerString  = "answer 8"
+            break;
+        case '9':
+            questionString  = "question 9"
+            answerString  = "answer 9"
+            break;
+        case '10':
+            questionString  = "question 10"
+            answerString  = "answer 10"
+            break;
+        case '11':
+            questionString  = "question 11"
+            answerString  = "answer 11"
+            break;
+        case '12':
+            questionString  = "question 12"
+            answerString  = "answer 12"
+            break;
+        
+            
+    }
+    question.innerText = questionString;
+    answer.innerText = answerString;
+    outermodal.classList.add('active');
+
+}
+
+function outsideClick(){
+    outermodal.classList.remove('active');
+}
+
+outermodal.addEventListener("click", function(event){
+    const isOutside = !event.target.closest('.inner-modal');
+    if(isOutside){
+        outsideClick();
+    }
+});
+
+
+function revealAnswer(){
+    
+    
+
+}
+revealAnswer.addEventListener("click", revealAnswer);
+
