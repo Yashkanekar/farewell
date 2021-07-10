8a305c0fe458569b3d8e4c63abf7cb9dfb5cde93
Yashkanekar <kanekaryash.11@gmail.com>
2021-07-10 22:55:28 +0530

    final changes

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const cardButtons = document.querySelectorAll('.btn');
 const outermodal = document.querySelector('.outer-modal');
 const innermodal = document.querySelector('.inner-modal');
-let questionString; 
+let questionString;
 let answerString;
 const revealAnswerButton = document.querySelector(".inner-btn");
-const answerSpan = document.querySelector(".hidden") ;
+const answerSpan = document.querySelector(".hidden");
 
 cardButtons.forEach((button) =>
     button.addEventListener("click", handleClick)
 );
+
 function handleClick(event) {
     const button = event.currentTarget;
     const card = button.closest('.card');
@@ -16,83 +17,87 @@ function handleClick(event) {
     const question = innermodal.querySelector(".question");
     const answer = innermodal.querySelector(".answer");
 
-    switch(number){
+    switch (number) {
         case '1':
-            questionString  = "Jack of all trades, Madhuri Dixit of SoW"
-            answerString  = "Sakshi Kadam!"
+            questionString = "Jack of all trades, Madhuri Dixit of SoW"
+            answerString = "Sakshi Kadam"
             break;
         case '2':
-            questionString  = "Mr.UPI, PLayboii, Keeper of secrets"
-            answerString  = "Shaad Qureshi!"
+            questionString = "Mr.UPI, PLayboii, Keeper of secrets"
+            answerString = "Shaad Qureshi"
             break;
         case '3':
-            questionString  = "Open hearted, Sasta Farhaan Akhtar"
-            answerString  = "Shashank Avhad!"
+            questionString = "Open hearted, Sasta Farhaan Akhtar"
+            answerString = "Shashank Avhad"
             break;
         case '4':
-            questionString  = "Night owl,Grandmaster, Temperamental"
-            answerString  = "Megh Kurlekar!"
+            questionString = "Night owl, Grandmaster, Temperamental"
+            answerString = "Megh Kurlekar"
             break;
         case '5':
-            questionString  = "Techie, Ekach chava, Intimidating"
-            answerString  = "Rohan Kokane!"
+            questionString = "Techie, Ekach chava, Intimidating"
+            answerString = "Rohan Kokane"
             break;
         case '6':
-            questionString  = "Beloved Sir, Very friendly, Never gives party"
-            answerString  = "Aayush Sir🙏"
+            questionString = "Beloved Sir, Very friendly, Never gives party"
+            answerString = "Aayush Sir🙏"
             break;
         case '7':
-            questionString  = "Content Creator, Never replies in the group"
-            answerString  = "Adesh Patki!"
+            questionString = "Content Creator, Never replies in the group"
+            answerString = "Adesh Patki"
             break;
         case '8':
-            questionString  = "Talkative, Has 1000 brothers except THE ONE"
-            answerString  = "Prithvi Dambal!"
+            questionString = "Talkative, Has 1000 brothers except THE ONE"
+            answerString = "Prithvi Dambal"
             break;
         case '9':
-            questionString  = "Jolly, Team person, Calm"
-            answerString  = "Rutuja Jadhav!"
+            questionString = "Jolly, Team person, Calm"
+            answerString = "Rutuja Jadhav"
             break;
         case '10':
-            questionString  = "Innocent, Password keeper, Smart"
-            answerString  = "Mrunal Kumbhar!"
+            questionString = "Innocent, Password keeper, Smart"
+            answerString = "Mrunal Kumbhar"
             break;
         case '11':
-            questionString  = "A head full of ideas, Miss Ghumakkad"
-            answerString  = "Janhavi Jadhav!"
+            questionString = "A head full of ideas, Miss Ghumakkad"
+            answerString = "Janhavi Jadhav"
             break;
         case '12':
-            questionString  = "Overdriven, Artistic, NRI"
-            answerString  = "Aryan Garg!"
+            questionString = "Overdriven, Artistic, NRI"
+            answerString = "Aryan Garg"
             break;
-        
-            
+
+
     }
     question.innerText = questionString;
     outermodal.classList.add('active');
 
-    function revealAnswer(){
+    function revealAnswer() {
 
         answerSpan.classList.remove("hidden")
         answerSpan.classList.add("answer");
-        answerSpan.innerText = answerString ;
-        
-        revealAnswerButton.classList.add("hide");        
+        answerSpan.innerText = answerString;
+
+        revealAnswerButton.classList.add("hide");
     }
     revealAnswerButton.addEventListener("click", revealAnswer);
-    
+
+    button.classList.add("opened");
+    button.innerText = "Opened";
+    card.classList.add("noTransition");
+    button.disabled = true;
 }
 
-function outsideClick(){
+function outsideClick() {
     answerSpan.classList.remove("answer")
     answerSpan.classList.add("hidden");
     outermodal.classList.remove('active');
     revealAnswerButton.classList.remove("hide");
 }
 
-outermodal.addEventListener("click", function(event){
+outermodal.addEventListener("click", function (event) {
     const isOutside = !event.target.closest('.inner-modal');
-    if(isOutside){
+    if (isOutside) {
         outsideClick();
     }
 });
